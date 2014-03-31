08269c1e76bb878e821ae0e4fe05de2ea69ca43c
Cheeseum <z.cheeseum@gmail.com>
2014-03-30 20:33:13 -0700

    cleaup

--- src/main/java/cheeseum/pistoneverything/PistonEverything.java
+++ src/main/java/cheeseum/pistoneverything/PistonEverything.java
@@ -39,7 +39,7 @@ public final class PistonEverything
 	// TODO: is it REALLY a good idea to pull these out instead of rolling them in bytecode?
 	public static void restoreStoredPistonBlock (World worldObj, int xCoord, int yCoord, int zCoord, int block, int meta, NBTTagCompound tileEntityData)
 	{
-		worldObj.setBlock(xCoord, yCoord, zCoord, block);
+	    worldObj.setBlock(xCoord, yCoord, zCoord, block);
 	    worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, meta, 3);
 	    worldObj.notifyBlockOfNeighborChange(xCoord, yCoord, zCoord, block);
 	
@@ -48,13 +48,6 @@ public final class PistonEverything
 	    te.yCoord = yCoord;
 	    te.zCoord = zCoord;
 	    worldObj.setBlockTileEntity(xCoord, yCoord, zCoord, te);
-/*    	tileEntityData.setInteger("x", xCoord);
-    	tileEntityData.setInteger("y", yCoord);
-    	tileEntityData.setInteger("z", zCoord);
-    	TileEntity te = worldObj.getBlockTileEntity(xCoord, yCoord, zCoord);
-    	te.readFromNBT(tileEntityData);
-    	te.blockMetadata = meta;
-*/
 	}
 
 	public static NBTTagCompound getBlockTileEntityData(World worldObj, int x, int y, int z)
